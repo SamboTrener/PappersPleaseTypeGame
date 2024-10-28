2e3ff6a0b559a4f892fcd722ae6f829ca5edc7c7
SamboTrener <trololo1247@list.ru>
2024-10-28 14:48:58 +0300

    ending

--- Assets/Scripts/UI/EndGameWindow.cs
+++ Assets/Scripts/UI/EndGameWindow.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,29 +8,104 @@ public class EndGameWindow : MonoBehaviour
 {
     public static EndGameWindow Instance { get; private set; }
 
-    [SerializeField] TextMeshProUGUI finalDescriptionText;
-    [SerializeField] GameObject goodEndingObject;
-    [SerializeField] GameObject badEningObject;
+    [SerializeField] TextMeshProUGUI textContainer;
+    [SerializeField] Image imageContainer;
+    [SerializeField] Sprite[] factoryEndingSprites;
+    [SerializeField] Sprite[] riftEndingSprites;
+    [SerializeField] Button nextButton;
+    [SerializeField] TextMeshProUGUI nextButtonText;
 
+    string[] endingText;
+    Sprite[] endingSprites;
+
+    int iter;
+    bool isFactoryEnding;
+    
 
     private void Awake()
     {
         Instance = this;
+        nextButton.onClick.AddListener(ShowNextEndingInfo);
         gameObject.SetActive(false);
     }
 
-    public void ShowGameEndWindow(bool isGoodEnding) 
+    public void ShowGameEndWindow(bool isFactoryEnding) 
     {
+        this.isFactoryEnding = isFactoryEnding;
+        iter = -1;
+        FillEndingData(isFactoryEnding);
         gameObject.SetActive(true);
-        if (isGoodEnding)
+        ShowNextEndingInfo();
+    }
+
+    void ShowNextEndingInfo()
+    {
+        ++iter;
+        if(iter > endingSprites.Length || iter > endingText.Length)
         {
-            goodEndingObject.SetActive(true);
-            finalDescriptionText.text = "Хорошая концовка";
+            nextButton.onClick.RemoveAllListeners();
+            nextButton.onClick.AddListener(() => SceneManager.LoadScene("Menu"));
+            if(YGManager.GetLanguageStr() == "ru")
+            {
+                nextButtonText.text = isFactoryEnding ? "Слава Заводу!" : "Слава Разлому!";
+            }
+            else
+            {
+                nextButtonText.text = isFactoryEnding ? "Glory to the Factory!" : "Glory to the Rift!";
+            }
         }
         else
         {
-            badEningObject.SetActive(true);
-            finalDescriptionText.text = "Плохая концовка";
+            textContainer.text = endingText[iter];
+            imageContainer.sprite = endingSprites[iter];
+        }
+    }
+
+    void FillEndingData(bool isFactoryEnding)
+    {
+        if(YGManager.GetLanguageStr() == "ru")
+        {
+            if (isFactoryEnding)
+            {
+                endingText = new string[] 
+                {
+                    "Вы ликвидировали представителя комитета Регулирования Аномальных Зон и Ликвидации Опасных Материй. " +
+                    "Позже выяснилось, что о таком комитете  не слышал ни один человек в государственном аппарате. " +
+                    "Сразу после ликвидации, большая группа, состоящая из (((них))), попыталась прорваться на Завод.",
+                    "Вы остановили прорыв ценой своей жизни и посмертно были награждены орденом третьей степени " +
+                    "“За защиту интересов человечества” директором Завода.",
+                    "Завод, в свою очередь, стал предприятием особой важности и продолжил свою деятельность в невиданных ранее масштабах",
+                    "После попытки провокации неизвестных сил, контроль за Заводом усилился в стократном размере. " +
+                    "Недоброжелатели извне стали говорить о том, что деятельность Завода использует Объект исключительно для создания " +
+                    "машин по порабощению населения и захвату соседних стран.",
+                    "Но слова завистников проходят мимо ушей порядочных граждан. " +
+                    "За вклад в наступление Великого Будущего, на территории вашего завода был поставлен памятник директору из " +
+                    "чистого золота высотой в 14 метров.",
+                    "В награду за вашу жертву на благо Завода, руководство позволило вам продолжить службу даже после вашей кончины. " +
+                    "Не без помощи Объекта, ваш уцелевший мозг был внедрен в механизм вечного поддержания жизни. " +
+                    "Вы стали бессмертным и бессменным вахтером Завода. Вы были удостоены чести до скончания веков охранять Завод от недоброжелателей. " +
+                    "Слава Заводу!"
+                };
+                endingSprites = factoryEndingSprites;
+            }
+            else
+            {
+                endingText = new string[] { };
+                endingSprites = riftEndingSprites;
+            }
+        }
+        else
+        {
+            if (isFactoryEnding)
+            {
+                endingText = new string[] { };
+                endingSprites = factoryEndingSprites;
+            }
+            else
+            {
+                endingText = new string[] { };
+                endingSprites = riftEndingSprites;
+            }
         }
     }
 
